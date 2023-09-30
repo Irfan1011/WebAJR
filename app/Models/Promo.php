f2977b7353438454239fc185625a4fd0f05681be
unknown <irfan10112001@gmail.com>
2023-10-01 00:40:14 +0700

    first commit

---
+++ app/Models/Promo.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Promo extends Model
+{
+    protected $table='promo';
+    protected $primarykey='promo_code';
+    public $timestamps = true;
+    protected $fillable = [
+        'promo_code',
+        'promo_type',
+        'description',
+    ];
+    // public function transaksi()
+    // {
+    //     return $this->hasMany(Transaksi::class);
+    // }
+}
